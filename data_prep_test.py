68efc5a3797caf2c28796ada667a956424508776
Unknown <tidi.stam@gmail.com>
2018-06-14 16:43:40 +0200

    Boiiiiiiiiii untangle functie werkt en heb de untangled_csv toegevoegd

--- data_prep_test.py
+++ data_prep_test.py
@@ -8,13 +8,17 @@ def main():
     dtypes = {'city_or_county': object, 'participant_age': object, 'participant_gender' : object, 'participant_status' : object, 'participant_type' : object}
     df = pd.read_csv('guns.csv')
     ndf = pd.read_csv('stripped2_guns.csv', dtype = dtypes)
-    remove(ndf)
+    # remove(ndf)
     # sufficient_data(ndf, "state")
     # string_to_int(ndf, "city_or_county")
     # string_to_int(ndf, "state")
     # edit_csv(ndf, "state")
     # edit_csv(ndf, "city_or_county")
     # header('stripped2_guns.csv')
+    list_of_dict = participant_untangle(ndf, 'participant_age', 'participant_gender', 'participant_type')
+    write_to_csv(list_of_dict)
+    
+    
 
 def header(csv_file):
     headers = pd.read_csv(csv_file, nrows = 1).columns
@@ -46,6 +50,10 @@ def edit_csv(df, column):
     df = pd.DataFrame.merge(df, df_column, how = 'left', left_on= column, right_on= None , right_index=True )
     df.to_csv("stripped2_guns.csv", index = False)
 
+def write_to_csv(list):
+    df = pd.DataFrame(list)
+    df.to_csv("participants_untangled.csv", index = False)
+
 # sla dict op in apart textbestand
 def save_dic(dictionary, column):
     with open(column+"_dict.txt", 'w') as file:
@@ -66,6 +74,69 @@ def remove(df):
     keep_col = ['incident_id', 'date', 'state', 'city_or_county', 'n_killed', 'n_injured', 'congressional_district', 'incident_characteristics', 'latitude', 'longitude', 'n_guns_involved', 'participant_age', 'participant_gender', 'participant_type', 'state_house_district', 'state_senate_district', 'state_id', 'city_or_county_id']
     df = df[keep_col]
     df.to_csv('stripped2_guns.csv', index = False)
+
+
+
+def participant_untangle(df, column1, column2, column3):
+    list1 = []
+    n = len(open("stripped2_guns.csv").readlines())
+    print(n)
+    for i in range(n-1):
+        row1 = df[column1][i]
+        row2 = df[column2][i]
+        row3 = df[column3][i]
+        # check of waarde niet NaN is en zo nee; verander syntax van de inhoud
+        if pd.notna(df[column1].iloc[i]):
+            if "::" in row1:
+                row1 = '{"' + row1.replace('||', '", "').replace('::', '":"') + '"}'
+            else:
+                row1 = '{"' + row1.replace('|', '", "').replace(':', '":"') + '"}'
+            h1 = json.loads(row1)
+        if pd.notna(df[column2].iloc[i]):
+            if "::" in row2:
+                row2 = '{"' + row2.replace('||', '", "').replace('::', '":"') + '"}'
+            else:
+                row2 = '{"' + row2.replace('|', '", "').replace(':', '":"') + '"}'
+            h2 = json.loads(row2)
+        if pd.notna(df[column3].iloc[i]):
+            if "::" in row3:
+                row3 = '{"' + row3.replace('||', '", "').replace('::', '":"') + '"}'
+            else:
+                row3 = '{"' + row3.replace('|', '", "').replace(':', '":"') + '"}'
+            h3 = json.loads(row3)
+        #else:
+            #if pd.notna(df[column1].iloc[i]):
+                #row1 = '{"' + row1.replace('|', '", "').replace(':', '":"') + '"}'
+                #h1 = json.loads(row1)
+            #if pd.notna(df[column2].iloc[i]):
+                #row2 = '{"' + row2.replace('|', '", "').replace(':', '":"') + '"}'
+                #h2 = json.loads(row2)
+            #if pd.notna(df[column3].iloc[i]):
+                #row3 = '{"' + row3.replace('|', '", "').replace(':', '":"') + '"}'
+                #h3 = json.loads(row3)
+
+        merged_dict = {}
+
+        # kijk hoe lang de langste lijst is zodat je langs alle keys gaat
+        for key in set(list(h1.keys()) + list(h2.keys()) + list(h3.keys())):
+            try:
+                merged_dict.setdefault(key,[]).append(h1[key])        
+            except KeyError:
+                pass
+            try:
+                merged_dict.setdefault(key,[]).append(h2[key])          
+            except KeyError:
+                pass
+            try:
+                merged_dict.setdefault(key,[]).append(h3[key])          
+            except KeyError:
+                pass
+        list1.append(merged_dict)
+        if (i % 1000==0):
+            print(i)
+    return(list1)
+        
+    
 
 if __name__ == "__main__":
     main()
